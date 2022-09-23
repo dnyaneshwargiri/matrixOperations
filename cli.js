2bd3dd7e4b1b49e7ffd77ae1ad725d3f0f0034ee
dnyaneshwargiri <dnyaneshwargiri510@gmail.com>
2022-09-23 13:25:07 +0530

    intial verify matrix function

--- cli.js
+++ cli.js
@@ -5,21 +5,43 @@ const fastcsv = require('fast-csv');
 
 let inputFile="/"+process.argv[2]
 let outputFile="/"+process.argv[3]
-var writeStream = fs.createWriteStream(__dirname+outputFile);
 
-var parser = parse.parse({columns: true}, function (err, records) {
-	console.log(records);
+
+function verifyMatrix(matrixList){
+	let length=JSON.parse(matrixList).length;
+ 	return length > 0 && Math.sqrt(length) % 1 === 0;
+} 
+
+function rotateMatrix(matrix){
+ console.log(matrix)
+}
+function getRotatedTable(matrix){
+	return verifyMatrix(matrix) ? rotateMatrix(matrix) : [[],false]
+}
+
+function writeCsv(){
+
+}
+
+const writeStream = fs.createWriteStream(__dirname+outputFile);
+const csvParser = parse.parse({columns: true}, function (err, records) {
+	//rotate each matrix
+	records.forEach(element => {
+		getRotatedTable(element.json)
+	});
 });
-fs.createReadStream(__dirname+inputFile).pipe(parser);
+//let writeCsv=fastcsv.write([ ["id","json" ],['3','"[6,7]"' ]],{headers:true}).pipe(writeStream)
 
-fastcsv.write([ ["id","json" ],['3','"[6,7]"' ]],{headers:true}).pipe(writeStream)
-
-
+fs.createReadStream(__dirname+inputFile).pipe(csvParser);
 
 
 
 
-// for(var i = 0; i < tempArray.length; i++){
+
+
+
+
+// for(const i = 0; i < tempArray.length; i++){
 //     fast_csv.write( [ tempArray[i]  ] );             //each element inside bracket
 //     }
 // fast_csv.end();
