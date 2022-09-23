fcb464e62eb3a2a4df1c16b27f60657b41d732a5
dnyaneshwar giri <dnyaneshwargiri510@gmail.com>
2022-09-23 16:27:53 +0530

    logic for formatting in output file

--- cli.js
+++ cli.js
@@ -6,7 +6,6 @@ const fastcsv = require('fast-csv');
 let inputFile="/"+process.argv[2]
 let outputFile="/"+process.argv[3]
 
-
 function verifyMatrix(matrixList){
 	let length=matrixList.length;
  	return length > 0 && Math.sqrt(length) % 1 === 0;
@@ -33,81 +32,81 @@ function formList(matrix,n,m){
 
 function rotateMatrixEdges(list){
 	let matrix=formMatrix(list)
-	let k=1 //rotate by 1
+	let k=1 //rotate by 1 clockwise
 	//n = rows
 	//m = columns
 	let n= matrix.length
 	let m= matrix[0].length
     let top=0, bottom=n-1,left=0,right=m-1;
-
     // Rotate while the ring exists.
     while(top<=bottom){
         // To hold the ring elements.
-        let elems=[];
+        let elements=[];
         // Do the spiral traversal and store the ring elements.
         for(let i=left;i<=right;i++){
-            elems.push(matrix[top][i]);
+            elements.push(matrix[top][i]);
         }
-        
         for(let i=top+1;i<=bottom;i++){
-            elems.push(matrix[i][right]);
+            elements.push(matrix[i][right]);
         }
-        
         for(let i=right-1;i>=left;i--){
-            elems.push(matrix[bottom][i]);
+            elements.push(matrix[bottom][i]);
         }
-        
         for(let i=bottom-1;i>top;i--){
-            elems.push(matrix[i][left]);
+            elements.push(matrix[i][left]);
         }
         // Check if the ring size is less than or equal to k
         // If true, break as the rings after will also be smaller than k.
-        if(elems.length<=k){
+        if(elements.length<=k){
             break;
         }
         // Rotation starts.
-        // ind represents the index of the position that should be at the start of the ring.
-        let sz=elems.length;
-        let ind=sz-k;
+        // index represents the index of the position that should be at the start of the ring.
+        let size=elements.length;
+        let index=size-k;
         // Store the rotated ring.
         for(let i=left;i<=right;i++){
-            matrix[top][i]=elems[ind];
-            ind++; ind%=sz;
+            matrix[top][i]=elements[index];
+            index++; index%=size;
         }
         for(let i=top+1;i<=bottom;i++){
-            matrix[i][right]=elems[ind];
-            ind++; ind%=sz;
+            matrix[i][right]=elements[index];
+            index++; index%=size;
         }
         for(let i=right-1;i>=left;i--){
-            matrix[bottom][i]=elems[ind];
-            ind++; ind%=sz;
+            matrix[bottom][i]=elements[index];
+            index++; index%=size;
         }
         for(let i=bottom-1;i>top;i--){
-            matrix[i][left]=elems[ind];
-            ind++; ind%=sz;
+            matrix[i][left]=elements[index];
+            index++; index%=size;
         }
         // Update the rotation parameters.
         top++; bottom--;
         left++; right--;
     }
-	return [formList(matrix,n,m),true]
+
+	return formList(matrix,n,m)
 }
-function getRotatedTable(list){
+
+function getRotatedTable(list,id){
 	list=JSON.parse(list)
-	return verifyMatrix(list) ? rotateMatrixEdges(list) : [[],false]
+	return verifyMatrix(list) ? [id,'['+rotateMatrixEdges(list)+']',true] : [id,"[]",false]
 }
 
-function writeCsv(){
-
-}
-
-const writeStream = fs.createWriteStream(__dirname+outputFile);
+data=[]
 const csvParser = parse.parse({columns: true}, function (err, records) {
 	//rotate each table
-	records.forEach(element => {
-		console.log(getRotatedTable(element.json),'\n');
+	records.forEach(element => {		
+		data.push(getRotatedTable(element.json,Number(element.id)))
 	});
+}).on("end",function(){
+	data.unshift(["id","json",'is_valid'])
+	const writeStream = fs.createWriteStream(__dirname+outputFile);
+	let writeCsv=fastcsv.write(data,{headers:true}).pipe(writeStream)
 });
-//let writeCsv=fastcsv.write([ ["id","json" ],['3','"[6,7]"' ]],{headers:true}).pipe(writeStream)
 
 fs.createReadStream(__dirname+inputFile).pipe(csvParser);
+
+
+
