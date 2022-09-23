a20d756fec3199cc28745fa0de498cbe557db9be
dnyaneshwargiri <dnyaneshwargiri510@gmail.com>
2022-09-23 12:45:03 +0530

    added write csv

--- cli.js
+++ cli.js
@@ -1,15 +1,25 @@
 const process = require('process');
-const csv = require('csv-stream')
 const fs = require('fs'); 
 const parse = require('csv-parse');
+const fastcsv = require('fast-csv');
 
 let inputFile="/"+process.argv[2]
-let outputFile=process.argv[3]
-
-
+let outputFile="/"+process.argv[3]
+var writeStream = fs.createWriteStream(__dirname+outputFile);
 
 var parser = parse.parse({columns: true}, function (err, records) {
 	console.log(records);
 });
-
 fs.createReadStream(__dirname+inputFile).pipe(parser);
+
+fastcsv.write([ ["id","json" ],['3','"[6,7]"' ]],{headers:true}).pipe(writeStream)
+
+
+
+
+
+
+// for(var i = 0; i < tempArray.length; i++){
+//     fast_csv.write( [ tempArray[i]  ] );             //each element inside bracket
+//     }
+// fast_csv.end();
